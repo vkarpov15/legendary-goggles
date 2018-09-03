5909ecfbc30b416c2740b4356783fe095551f52b
Valeri Karpov <val@karpov.io>
2018-09-02 20:04:33 -0400

    use http instead of superagent for better streaming

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const assert = require('assert');
 const config = require('./.config');
 const findUpdates = require('./lib/findUpdates');
 const { githubToSlack } = require('@atomist/slack-messages/Markdown');
+const http = require('http');
 const parseChangelog = require('./lib/parseChangelog');
 const superagent = require('superagent');
 
-const server = 'https://skimdb.npmjs.com/registry';
+const server = 'registry.npmjs.org';
 
 run().catch(error => console.error(error.stack));
 
@@ -19,11 +20,36 @@ async function run() {
 //deps('mongoose', '5.2.9').catch(error => console.error(error.stack));
 
 async function getLatestChangelog(pkg) {
-  const { versions, repository } = await superagent.get(`${server}/${pkg}`).
-    then(res => res.body);
+  // Use native HTTP because superagent struggles with application/octet-stream,
+  // which is what npm gives you for very large packages.
+  // See https://github.com/visionmedia/superagent/issues/402
+  const res = await new Promise((resolve, reject) => {
+    const opts = {
+      method: 'GET',
+      hostname: server,
+      path: `/${pkg}`
+    };
+    let str = '';
+    const request = http.request(opts, res => {
+      res.setEncoding('utf8');
+      res.on('data', chunk => {
+        str += chunk.toString('utf8');
+      });
+      res.on('end', () => {
+        resolve(JSON.parse(str))
+      });
+      res.on('error', err => reject(err));
+    });
+
+    request.on('error', error => reject(error));
+    request.end();
+  });
+
+  const { versions, repository } = res;
   const version = Object.keys(versions).reverse()[0];
 
   const githubUrl = convertToGithubUrl(repository);
+  console.log('Getting changelog');
   const { changelog, url } = await getChangelog(githubUrl);
   const parsed = parseChangelog(changelog);
   return { changelog: parsed[version], version, url };
