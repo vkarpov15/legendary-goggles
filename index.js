e10f9ec8ac9296d185316330f19e3520bb9fde88
Valeri Karpov <val@karpov.io>
2018-09-10 10:31:01 -0400

    refactor: separate out a bunch of functions

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Slack = require('node-slack');
 const assert = require('assert');
 const config = require('./.config');
 const convertToGithubUrl = require('./lib/helpers/convertToGithubUrl');
 const findUpdates = require('./lib/findUpdates');
-const { githubToSlack } = require('@atomist/slack-messages/Markdown');
+const getChangelog = require('./lib/getChangelog');
 const http = require('http');
 const parseChangelog = require('./lib/parseChangelog');
+const postToSlack = require('./lib/postToSlack');
 const superagent = require('superagent');
 
 const server = 'registry.npmjs.org';
@@ -13,13 +13,15 @@ const server = 'registry.npmjs.org';
 run().catch(error => console.error(error.stack));
 
 async function run() {
-  const pkg = 'run-rs';
-  const { changelog, version, url } = await getLatestChangelog(pkg);
+  const updated = await findUpdates();
 
-  await postToSlack(pkg, version, url, changelog);
+  for (const pkg of updated) {
+    console.log('Found', pkg)
+    const { changelog, version, url } = await getLatestChangelog(pkg);
+
+    await postToSlack(pkg, version, url, changelog);
+  }
 }
-
-//deps('mongoose', '5.2.9').catch(error => console.error(error.stack));
 
 async function getLatestChangelog(pkg) {
   // Use native HTTP because superagent struggles with application/octet-stream,
@@ -55,62 +57,4 @@ async function getLatestChangelog(pkg) {
   const { changelog, url } = await getChangelog(githubUrl);
   const parsed = parseChangelog(changelog);
   return { changelog: parsed[version], version, url };
-}
-
-async function postToSlack(pkg, version, url, changelog) {
-  const webhook = config.slackWebhook;
-  const slack = new Slack(webhook, {});
-
-  changelog = githubToSlack(changelog);
-
-  const lines = changelog.split('\n');
-  for (let i = 0; i < lines.length; ++i) {
-    if (lines[i].startsWith('#')) {
-      lines[i] = `*${lines[i].replace(/^#+/, '')}*`;
-    }
-    if (i + 1 < lines.length && /^[-=]+$/.test(lines[i + 1].trim())) {
-      lines[i] = `*${lines[i]}*`;
-      lines[i + 1] = '';
-    }
-  }
-
-  changelog = lines.join('\n');
-
-  await slack.send({
-    text: `${pkg} v${version} released. <${url}|Changelog>:\n\n${changelog}`,
-    channel: '#test',
-    icon: ':mongoose:'
-  });
-}
-
-async function deps(pkg, version) {
-  const { versions, repository } = await superagent.get(`${server}/${pkg}`).
-    then(res => res.body);
-  const { dependencies } = versions[version];
-  console.log(dependencies, repository);
-
-  const githubUrl = convertToGithubUrl(repository);
-  const changelog = await getChangelog(githubUrl);
-
-  const parsed = parseChangelog(changelog);
-  console.log(parsed[version]);
-}
-
-async function getChangelog(url) {
-  const allowed = ['History.md', 'HISTORY.md', 'CHANGELOG.md'];
-
-  const originalUrl = url;
-
-  url = url.replace('github.com', 'raw.githubusercontent.com');
-
-  for (const file of allowed) {
-    const md = await superagent.get(`${url}/master/${file}`).
-      then(res => res.text).
-      catch(() => null);
-    if (md != null) {
-      return { changelog: md, url: `${originalUrl}/blob/master/${file}` };
-    }
-  }
-
-  return null;
 }
