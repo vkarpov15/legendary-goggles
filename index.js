ae960bb9e2de8555452b30915e184a8bcf0d942a
Valeri Karpov <val@karpov.io>
2018-09-11 17:44:00 -0400

    feat: start storing packages in mongodb

--- index.js
+++ index.js
@@ -1,22 +1,85 @@
+const chalk = require('chalk');
+const config = require('./.config');
 const convertToGithubUrl = require('./lib/helpers/convertToGithubUrl');
+const dbConnect = require('./lib/mongoose');
 const findUpdates = require('./lib/findUpdates');
 const getChangelog = require('./lib/getChangelog');
 const http = require('http');
+const moment = require('moment');
 const parseChangelog = require('./lib/parseChangelog');
 const postToSlack = require('./lib/postToSlack');
+const superagent = require('superagent');
 
 const server = 'registry.npmjs.org';
 
 run().catch(error => console.error(error.stack));
 
 async function run() {
-  const updated = await findUpdates();
+  const db = await dbConnect(config.mongodb);
 
-  for (const pkg of updated) {
-    const { changelog, version, url } = await getLatestChangelog(pkg);
+  const Package = db.model('Package');
+  const State = db.model('State');
+  // const Version = db.model('Version');
 
-    await postToSlack(pkg, version, url, changelog);
+  const opts = { new: true, upsert: true, setDefaultsOnInsert: true };
+  const state = await State.findOneAndUpdate({}, {}, opts);
+
+  while (true) {
+    console.log(ts(), `Start loop at ${state.lastSequenceNumber}`);
+    const { updated, lastSequenceNumber } =
+      await findUpdates(state.lastSequenceNumber);
+
+    for (const name of updated) {
+      console.log(ts(), `Updated package "${name}"`);
+      const npmData = await superagent.get(`https://${server}/${name}`).
+        then(res => res.body);
+
+      npmData['distTags'] = npmData['dist-tags'];
+      for (const key of Object.keys(npmData['distTags'])) {
+        let sanitized = key;
+        if (key.includes('.')) {
+          sanitized = key.replace(/\./g, '-');
+        }
+        if (key.startsWith('$')) {
+          sanitized = key.substr(1);
+        }
+        if (key !== sanitized) {
+          npmData['distTags'][sanitized] = npmData['distTags'][key];
+          delete npmData['distTags'][key];
+        }
+      }
+      const versionDetail = npmData['versions'];
+      npmData['versions'] = Object.keys(versionDetail);
+      delete npmData.readme;
+
+      console.log(ts(), npmData);
+
+      let pkg = await Package.findOne({ _id: name });
+
+      if (pkg == null) {
+        pkg = new Package(npmData);
+        await pkg.save();
+      }
+
+      const { changelog, version, url, githubUrl } = await getLatestChangelog(name);
+
+      console.log(ts(), changelog);
+
+      await postToSlack(name, version, url, changelog, githubUrl);
+    }
+
+    state.lastSequenceNumber = lastSequenceNumber;
+    await state.save();
+
+    console.log(ts(), 'Done with this loop, waiting 5 mins');
+
+    // Wait 5 minutes
+    await new Promise(resolve => setTimeout(resolve, 5 * 60 * 1000));
   }
+}
+
+function ts() {
+  return chalk.blue(moment().format('YYYYMMDD HH:mm:ss'));
 }
 
 async function getLatestChangelog(pkg) {
@@ -48,9 +111,19 @@ async function getLatestChangelog(pkg) {
   const { versions, repository } = res;
   const version = Object.keys(versions).reverse()[0];
 
-  const githubUrl = convertToGithubUrl(repository);
-  console.log('Getting changelog');
-  const { changelog, url } = await getChangelog(githubUrl);
+  let githubUrl;
+  try {
+    githubUrl = convertToGithubUrl(repository);
+  } catch (error) {
+    return { changelog: null, version, url: null, githubUrl: null };
+  }
+  const changelogInfo = await getChangelog(githubUrl);
+
+  if (changelogInfo == null) {
+    return { changelog: null, version, url: null, githubUrl };
+  }
+
+  const { changelog, url } = changelogInfo;
   const parsed = parseChangelog(changelog);
-  return { changelog: parsed[version], version, url };
+  return { changelog: parsed[version], version, url, githubUrl };
 }
