91ee2362406dd13840689fbaed3bccf7fb6bbbd3
Valeri Karpov <val@karpov.io>
2018-09-29 11:32:45 -0400

    fix(updatePackage): return correct value

---
+++ test/updatePackage.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Library = require('../lib');
+const assert = require('assert');
+const http = require('../lib/util/get');
+const logger = require('../lib/logger');
+const sinon = require('sinon');
+
+if (!process.env.D) {
+  logger.transports = [];
+}
+
+describe('updatePackage', function() {
+  let lib;
+
+  before(async function() {
+    lib = await Library({ mongodb: 'mongodb://localhost:27017/jsreport' });
+
+    await lib.db.dropDatabase();
+  });
+
+  after(function() {
+    lib.db.close();
+  });
+
+  it('works', async function() {
+    const pkgData = require('./data/pkg');
+    const stub = sinon.stub(http, 'get');
+    stub.onFirstCall().returns(pkgData);
+    stub.returns(null);
+
+    const { pkg, newVersions } = await lib.updatePackage('print-pkg-version');
+
+    assert.ok(pkg);
+    assert.equal(newVersions.length, 3);
+
+    assert.deepEqual(newVersions.map(v => v.version).sort(),
+      ['0.1.0', '0.2.0', '0.2.1']);
+  });
+
+  afterEach(function() {
+    http.get.restore && http.get.restore();
+  });
+});
