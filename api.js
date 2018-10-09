69c386bc79d3fb15457929b1dc8fa8f8475a4dd9
Valeri Karpov <val@karpov.io>
2018-10-09 10:45:22 -0400

    chore: add stubbed out slack oauth

---
+++ api.js
@@ -0,0 +1,14 @@
+const api = require('./lib/api');
+const connect = require('./lib');
+
+run().catch(error => console.error(error.stack));
+
+async function run() {
+  const lib = await connect(require('./.config'));
+  const app = await api(lib);
+
+  const port = process.env.PORT || 80;
+  await app.listen(port);
+
+  console.log(`Listening on port ${port}`);
+}
