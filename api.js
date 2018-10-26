9e9091267a555e25cac9bc43ab80247c1938ecc1
Valeri Karpov <val@karpov.io>
2018-10-26 09:50:00 -0400

    feat: add updateAccount

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ run().catch(error => console.error(error.stack));
 
 async function run() {
   const lib = await connect(require('./.config'));
+
   const app = await api(lib);
 
   const port = process.env.PORT || 80;
